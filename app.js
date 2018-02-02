861155fc5f8671facf4f12b22bba7422d47998e1
mrwirawat <weerawatposriya@gmail.com>
2018-02-02 16:20:20 +0700

    add feature
    - add opacity to add color
    - add eraser

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var color = $(".selected").css("background-color");
 var $canvas = $("canvas");
 var context = $("canvas")[0].getContext("2d");
-var lastEvent; 
+var lastEvent;
 var mouseDown = false;
 
 
@@ -9,67 +9,66 @@ var mouseDown = false;
 $(".controls").on("click", "li", function(){
    //Deselect siblingelements
   $(this).siblings().removeClass("selected");
-  
+
   //Select clicked element
   $(this).addClass("selected");
-  
+
   //cache current color
   color = $(this).css("background-color");
 });
 
- 
+
 
 //When add color is pressed
 $("#revealColorSelect").click(function() {
   //Show color select or hide the color select
-  $("#colorSelect").toggle(500);
+  $("#colorSelect").slideToggle(1000);
 });
-  
+
 //Update the new color span
 function changeColor() {
  var r = $("#red").val();
  var g = $("#green").val();
- var b = $("#blue").val(); 
- $("#newColor").css("background-color", "rgb(" + r +  "," + g + "," + b + ")"); 
+ var b = $("#blue").val();
+ var a = $("#alpha").val();
+ $("#newColor").css("background-color", "rgba(" + r +  "," + g + "," + b + "," + a/100 +")");
 }
 
 //When color sliders change
- $("input[type=range]").change(changeColor);                   
-  
+ $("input[type=range]").change(changeColor);
+
 
 //When add color is pressed
 $("#addNewColor").click(function(){
    //Append the color to the control ul
-  var $newColor = $ ("<li></li>");
+  var $newColor = $("<li></li>");
   $newColor.css("background-color", $("#newColor").css("background-color"));
-$(".controls ul").append($newColor);
-   
+  $(".controls ul").append($newColor);
   //Select the new color
   $newColor.click();
 });
- 
+
 
 //On mouse events on the canvas
 $canvas.mousedown(function(e) {
   lastEvent = e;
   mouseDown = true;
 }).mousemove(function(e) {
-
   //Draw lines
   if(mouseDown){
-  context.beginPath();
-  context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
-  context.lineTo(e.offsetX, e.offsetY);
-  context.strokeStyle = color;
-  context.stroke();
-  lastEvent = e;
+    if (color == "rgb(255, 255, 255)") {
+      context.clearRect(e.offsetX-50, e.offsetY-50, 100, 100);//eraser
+    }else{
+      context.beginPath();
+      context.moveTo(lastEvent.offsetX, lastEvent.offsetY);//draw begin
+      context.lineTo(e.offsetX, e.offsetY);//draw end
+      context.strokeStyle = color;//pick color
+      context.stroke();
+      lastEvent = e;
+    }
   }
 }).mouseup(function(){
   mouseDown = false;
 }).mouseleave(function(){
   $canvas.mouseup();
 });
-
-
-
-
